c217ac28ea97fc931a3af9afd2a08775e3d70442
Alvaro Pinot <alvaro.pinot@adesis.com>
2014-12-05 14:56:07 +0100

    update(menu.js) name corrected, added points

--- src/js/menu.js
+++ src/js/menu.js
@@ -10,15 +10,22 @@
 
     create: function () {
       var x = this.game.width / 2
-        , y = this.game.height / 2;
+        , y = this.game.height / 2,
+        margin = 20;
 
 
-      this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'Example Game' );
+      y = y - 120;
+      this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'pixElement');
       this.titleTxt.align = 'center';
       this.titleTxt.x = this.game.width / 2 - this.titleTxt.textWidth / 2;
 
-      y = y + this.titleTxt.height + 5;
+      y = y + this.titleTxt.height + margin;
       this.startTxt = this.add.bitmapText(x, y, 'minecraftia', 'START');
+      this.startTxt.align = 'center';
+      this.startTxt.x = this.game.width / 2 - this.startTxt.textWidth / 2;
+
+      y = y + this.titleTxt.height + margin*2;
+      this.startTxt = this.add.bitmapText(x, y, 'minecraftia', 'last score:\n ' + this.game.points + ' points');
       this.startTxt.align = 'center';
       this.startTxt.x = this.game.width / 2 - this.startTxt.textWidth / 2;
 
